54e48cb65fa86ac07a51b6df0ff0b352ec3a33e0
Volodymyr Shatskyi <shockone89@gmail.com>
2017-04-06 19:10:27 +0300

    Dont hardcode language file path.

--- expand.rb
+++ expand.rb
@@ -1,8 +1,9 @@
 require 'json'
 STDOUT.sync = true
+language_file_path = ARGV.first
 
 while true
   abbreviation = $stdin.gets.chomp
-  language = JSON.parse(File.read('language.json'))
+  language = JSON.parse(File.read(language_file_path))
   puts language[abbreviation] || "#{abbreviation} is not defined"
 end
