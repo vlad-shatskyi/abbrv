48f74dd21e168f7579ba5048f4eb917a923b88df
Volodymyr Shatskyi <shockone89@gmail.com>
2017-04-07 19:10:24 +0300

    Make capturer a class.

--- capture.rb
+++ capture.rb
@@ -1,51 +1,61 @@
 require 'pty'
 STDOUT.sync = true
 
-cmd = "stdbuf -oL -- libinput-debug-events --show-keycodes --device /dev/input/event0 | awk -F' ' '{ print $4, $6}'"
+class Capturer
+  CAPTURE_COMMAND = "stdbuf -oL -- libinput-debug-events --show-keycodes --device /dev/input/event0 | awk -F' ' '{ print $4, $6}'"
 
-def alt?(key)
-  key == 'KEY_LEFTALT' || key == 'KEY_RIGHTALT'
-end
-
-def allowed_letter?(key)
-  '-=qwertyuiop[]asdfghjkl;\'zxcvbnm,./'.include?(key)
-end
-
-def to_letter(libnotify_key_name)
-  key_name = libnotify_key_name.split('_').last.downcase
-  case key_name
-  when 'equal'
-    '='
-  when 'minus'
-    '-'
-  when 'semicolon'
-    ';'
-  else
-    key_name
+  def initialize
+    @is_alt_pressed = false
+    @abbreviation = ''
   end
-end
 
-is_alt_pressed = false
-abbreviation = ''
+  def on_abbreviation
+    PTY.spawn(CAPTURE_COMMAND) do |stdout, _, _|
+      stdout.each do |line|
+        key, action = line.split(' ')
 
-PTY.spawn(cmd) do |stdout, _, _|
-  stdout.each do |line|
-    key, action = line.split(' ')
+        if @is_alt_pressed
+          letter = to_letter(key)
+          if action == 'pressed' && allowed_letter?(letter)
+            @abbreviation += letter
+          elsif action == 'released' && alt?(key)
+            if @abbreviation.size > 0
+              yield @abbreviation
+            end
 
-    if is_alt_pressed
-      letter = to_letter(key)
-      if action == 'pressed' && allowed_letter?(letter)
-        abbreviation += letter
-      elsif action == 'released' && alt?(key)
-        if abbreviation.size > 0
-          puts abbreviation
+            @is_alt_pressed = false
+            @abbreviation = ''
+          end
+        elsif action == 'pressed' && alt?(key)
+          @is_alt_pressed = true
         end
-
-        is_alt_pressed = false
-        abbreviation = ''
       end
-    elsif action == 'pressed' && alt?(key)
-      is_alt_pressed = true
     end
   end
+
+  def alt?(key)
+    key == 'KEY_LEFTALT' || key == 'KEY_RIGHTALT'
+  end
+
+  def allowed_letter?(key)
+    '-=qwertyuiop[]asdfghjkl;\'zxcvbnm,./'.include?(key)
+  end
+
+  def to_letter(libnotify_key_name)
+    key_name = libnotify_key_name.split('_').last.downcase
+    case key_name
+    when 'equal'
+      '='
+    when 'minus'
+      '-'
+    when 'semicolon'
+      ';'
+    else
+      key_name
+    end
+  end
+end
+
+Capturer.new.on_abbreviation do |abbreviation|
+  puts abbreviation
 end
