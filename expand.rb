8b12d7af3bddf25969f8258fead8a35190334c25
Volodymyr Shatskyi <shockone89@gmail.com>
2017-04-07 19:20:01 +0300

    make expander a class.

--- expand.rb
+++ expand.rb
@@ -2,8 +2,19 @@ require 'json'
 STDOUT.sync = true
 language_file_path = ARGV.first
 
+class Expander
+  def initialize(language_file_path)
+    @language_file_path = language_file_path
+  end
+
+  def expand(abbreviation)
+    language = JSON.parse(File.read(@language_file_path))
+    language[abbreviation] || "#{abbreviation} is not defined"
+  end
+end
+
+expander = Expander.new(language_file_path)
+
 while true
-  abbreviation = $stdin.gets.chomp
-  language = JSON.parse(File.read(language_file_path))
-  puts language[abbreviation] || "#{abbreviation} is not defined"
+  puts expander.expand($stdin.gets.chomp)
 end
