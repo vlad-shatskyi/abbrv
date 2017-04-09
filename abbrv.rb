53bfe5f72aa91c1d49648faf3aa510ca098a376c
Volodymyr Shatskyi <shockone89@gmail.com>
2017-04-09 09:27:34 +0300

    Add abbrv.rb

--- abbrv.rb
+++ abbrv.rb
@@ -1,1 +1,11 @@
 require_relative 'capture'
+require_relative 'expand'
+require_relative 'perform'
+
+language_file_path = ARGV.first
+expander = Expander.new(language_file_path)
+performer = Performer.new
+
+Capturer.new.on_abbreviation do |abbreviation|
+  performer.perform(expander.expand(abbreviation))
+end
